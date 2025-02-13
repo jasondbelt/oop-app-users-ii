b098febe51b538078043606d87d472842d88b7ba
Jason Belt <jasondbelt@gmail.com>
2025-02-13 10:23:57 -0600

    test cases not passing

--- User.py
+++ User.py
@@ -4,11 +4,11 @@ class User:
     posts = []
 
     # add specified instance attributes
-    def __init__(self, name, email, drivers_license, User_posts=[]):
+    def __init__(self, name, email, drivers_license):
         self.Name = name
         self.Email = email
         self.Drivers_license = drivers_license
-        self.User_posts = User_posts
+        self.User_posts = []
 
     # print string representation of objects
     def __repr__(self):
@@ -41,6 +41,7 @@ class User:
 
     def delete_a_post(self):
         deleted_post = []
+        # deleting by title
         title = input("Please enter post title you want to delete: ")
         
         try:
@@ -49,22 +50,26 @@ class User:
                 #if title and object title matches
                 if User.posts[i][(str(i+1))][0] == title:
                     deleted_post.append(User.posts[i])
+            if len(deleted_post) == 0:
+                print("Post Doesn't Exist")
             # actually deletes post from class and instance variable
             for item in deleted_post:
                 while item in User.posts:
                     User.posts.remove(item)
                     self.User_posts.remove(item)
                     print("Post Deleted Successfully")
-        except KeyError:
-            print("Post not found to delete")
+        except ValueError:
+            User.posts.append(deleted_post)
+            self.User_posts.append(deleted_post)
+            print("Can't Delete Someone Else's Post")
             
 # input = iter(["Johns †i†le", "I Just joined OOPX"])
 # user = User("John", "john@email.com", "FDUI87")
 # user2 = User("Jason", "jason@email.com", "12345")
-# user.create_a_post() # creating posts for all User instances, not just current instance
+# user.create_a_post() 
 # user2.create_a_post()
-# user.see_my_posts() # sees all posts regardless
-# user.see_all_posts() # sees all posts
+# user.see_my_posts() 
+# user.see_all_posts() 
 # user.delete_a_post()
 # user.see_all_posts()
 
